1a6de5af7e0cc52a97e168d574224b3e075d1b1f
qaboxletstest <qaboxletstest@gmail.com>
2021-10-15 15:00:03 +0530

    XML Response Support Added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const simpleAuth = require('./middleware/authentication')
 const logger = require('./middleware/logger')
 const fileUpload = require('express-fileupload')
 const rateLimit = require("express-rate-limit");
+const js2xmlparser = require("js2xmlparser");
+
 
 const app = express()
 const PORT = process.env.PORT || 5002
@@ -34,6 +36,24 @@ const createAccountLimiter = rateLimit({
 
 // Init Basic Authentication Middleware
 app.use(simpleAuth)
+
+// XML Middleware
+app.use(function (req, res, next) {
+    res.sendData = function (obj) {
+        if (req.accepts('json')) {
+            res.header('Content-Type', 'application/json');
+            res.send(obj);
+        } else if (req.accepts('application/xml')) {
+
+            res.header('Content-Type', 'application/xml');
+            var xml = js2xmlparser.parse("response", obj);
+            res.send(xml);
+        } else {
+            res.send(406);
+        }
+    }
+    next();
+});
 
 // Body Parser Middleware
 app.use(express.json())
--- routes/api/members.js
+++ routes/api/members.js
@@ -17,15 +17,15 @@ router.get('/', (req, res) => {
             const found = members.some(member => member.gender.toLowerCase() === gn.toLowerCase())
             if (found) {
                 const member = members.filter(member => member.gender.toLowerCase() === gn.toLowerCase())
-                res.json(member.sort((m1, m2) => m1.id - m2.id))
+                res.sendData(member.sort((m1, m2) => m1.id - m2.id))
             } else {
-                res.status(404).json({
+                res.status(404).sendData({
                     msg: `Member with gender ${gn} doesn't exist`
                 })
             }
         }
     }
-    res.json(members.sort((m1, m2) => m1.id - m2.id))
+    res.sendData(members.sort((m1, m2) => m1.id - m2.id))
 })
 
 // Get Single Member
@@ -33,9 +33,9 @@ router.get('/:id', (req, res) => {
     const found = members.some(member => member.id === parseInt(req.params.id))
     if (found) {
         const member = members.filter(member => member.id === parseInt(req.params.id))
-        res.json(member[0])
+        res.sendData(member[0])
     } else {
-        res.status(404).json({
+        res.status(404).sendData({
             msg: `Member with id ${req.params.id} doesn't exist`
         })
     }
@@ -49,34 +49,34 @@ router.post('/', (req, res) => {
         gender: req.body.gender
     }
     if (Object.keys(req.body).length > 2) {
-        return res.status(400).json({
+        return res.status(400).sendData({
             msg: "Please provide only name and gender"
         })
     } else if (!newMember.name || !newMember.gender) {
-        return res.status(400).json({
+        return res.status(400).sendData({
             msg: "Please provide a name and gender"
         })
     } else if (newMember.name.trim().length < 4 || newMember.name.trim().length > 25) {
-        return res.status(400).json({
+        return res.status(400).sendData({
             msg: "Name should be 4 to 25 characters long"
         })
     } else if (!genders.includes((newMember.gender).toLowerCase())) {
-        return res.status(400).json({
+        return res.status(400).sendData({
             msg: "Gender should be either male or female only"
         })
     } else if (!re.test(newMember.name)) {
-        return res.status(400).json({
+        return res.status(400).sendData({
             msg: "Name should only contain Alphabets"
         })
     }
     members.push(newMember)
     fs.writeFile(filePath, JSON.stringify(members.sort((m1, m2) => m1.id - m2.id), null, 2), err => {
         if (err) {
-            res.status(500).json({
+            res.status(500).sendData({
                 msg: `Internal Server Error while writing data to file`
             })
         } else {
-            res.status(201).json(newMember)
+            res.status(201).sendData(newMember)
         }
     })
 })
@@ -87,19 +87,19 @@ router.put('/:id', (req, res) => {
     if (found) {
         const updmember = req.body
         if (Object.keys(req.body).length > 2) {
-            return res.status(400).json({
+            return res.status(400).sendData({
                 msg: "Please provide only name and gender"
             })
         } else if (updmember.hasOwnProperty('name') && (updmember.name.trim().length < 4 || updmember.name.trim().length > 25)) {
-            return res.status(400).json({
+            return res.status(400).sendData({
                 msg: "Name should be 4 to 25 characters long"
             })
         } else if (updmember.hasOwnProperty('gender') && !genders.includes((updmember.gender).toLowerCase())) {
-            return res.status(400).json({
+            return res.status(400).sendData({
                 msg: "Gender should be either male or female only"
             })
         } else if (updmember.hasOwnProperty('name') && !re.test(updmember.name)) {
-            return res.status(400).json({
+            return res.status(400).sendData({
                 msg: "Name should only contain Alphabets"
             })
         }
@@ -109,11 +109,11 @@ router.put('/:id', (req, res) => {
                 member.gender = updmember.gender ? updmember.gender : member.gender
                 fs.writeFile(filePath, JSON.stringify(members.sort((m1, m2) => m1.id - m2.id), null, 2), err => {
                     if (err) {
-                        res.status(500).json({
+                        res.status(500).sendData({
                             msg: `Internal Server Error while writing data to file`
                         })
                     } else {
-                        res.json({
+                        res.sendData({
                             msg: `Member with id ${req.params.id} is updated successfully`,
                             member: member
                         })
@@ -122,7 +122,7 @@ router.put('/:id', (req, res) => {
             }
         })
     } else {
-        res.status(404).json({
+        res.status(404).sendData({
             msg: `Member with id ${req.params.id} doesn't exist`
         })
     }
@@ -135,18 +135,18 @@ router.delete('/:id', (req, res) => {
         members.splice(getIndexOfMember(members, req.params.id) + 1, 1)
         fs.writeFile(filePath, JSON.stringify(members.sort((m1, m2) => m1.id - m2.id), null, 2), err => {
             if (err) {
-                res.status(500).json({
+                res.status(500).sendData({
                     msg: `Internal Server Error while writing data to file`
                 })
             } else {
-                res.json({
+                res.sendData({
                     msg: `Member with id ${req.params.id} is deleted successfully`,
                     members: members.sort((m1, m2) => m1.id - m2.id)
                 })
             }
         })
     } else {
-        res.status(404).json({
+        res.status(404).sendData({
             msg: `Member with id ${req.params.id} doesn't exist`
         })
     }
