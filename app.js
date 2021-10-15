204d3d908576055d54c05cbb682cfb52040ee8f4
qaboxletstest <qaboxletstest@gmail.com>
2021-10-15 12:19:36 +0530

    Added Rate Limit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const simpleAuth = require('./middleware/authentication')
 const logger = require('./middleware/logger')
 const fileUpload = require('express-fileupload')
+const rateLimit = require("express-rate-limit");
 
 const app = express()
 const PORT = process.env.PORT || 5002
@@ -12,6 +13,14 @@ const PORT = process.env.PORT || 5002
 
 // Init middleware
 app.use(logger)
+
+// Rate-Limit
+const createAccountLimiter = rateLimit({
+    windowMs: 10000, // 10 Seconds
+    max: 2, // start blocking after 2 requests
+    message:
+        "Too many accounts created from this IP, please try again after 10 Seconds"
+});
 
 // STEP 1
 // app.get('/', (req, res) => {
@@ -32,7 +41,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(fileUpload())
 
 // API Members - persistence
-app.use('/api/members', require('./routes/api/members'))
+app.use('/api/members', createAccountLimiter, require('./routes/api/members'))
 
 // API File Upload
 app.use('/api/upload', require('./routes/api/fileupload'))
